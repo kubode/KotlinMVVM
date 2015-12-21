281db19fc5e77604b9ade7a5509c3747f7185794
久保出雅俊 <kubode@team-lab.com>
2015-12-21 21:16:37 +0900

    オーバーロードメソッド化

--- app/src/main/kotlin/com/teamlab/kotlin/mvvm/RxPropertyBinder.kt
+++ app/src/main/kotlin/com/teamlab/kotlin/mvvm/RxPropertyBinder.kt
@@ -2,27 +2,21 @@ package com.teamlab.kotlin.mvvm
 
 import android.widget.EditText
 import com.jakewharton.rxbinding.widget.textChanges
-import rx.Observable
 import rx.Subscription
 import rx.android.schedulers.AndroidSchedulers
 
-fun <T> Observable<T>.observeOnMainThread() = observeOn(AndroidSchedulers.mainThread())
-
-fun <V, T> V.bindRxProperty(property: ReadOnlyRxProperty<T>, setter: V.(T) -> Unit): Subscription {
+fun <V, T> V.bindRxProperty(property: ReadOnlyRxProperty<T>,
+                            initializer: V.(T) -> Unit,
+                            onNext: V.(T) -> Unit): Subscription {
     val subscription = property.publishSubject
-            .observeOnMainThread()
-            .subscribe { setter(it) }
-    setter(property.value)
-    return subscription
-}
-
-fun <V, T> V.bindRxProperty(property: ReadOnlyRxProperty<T>, initializer: V.(T) -> Unit, onNext: V.(T) -> Unit): Subscription {
-    val subscription = property.publishSubject
-            .observeOnMainThread()
+            .observeOn(AndroidSchedulers.mainThread())
             .subscribe { onNext(it) }
     initializer(property.value)
     return subscription
 }
+
+fun <V, T> V.bindRxProperty(property: ReadOnlyRxProperty<T>,
+                            setter: V.(T) -> Unit) = bindRxProperty(property, setter, setter)
 
 fun EditText.bindTextChanges(property: RxProperty<CharSequence>): Subscription {
     val subscription = textChanges().subscribe { property.value = it }
