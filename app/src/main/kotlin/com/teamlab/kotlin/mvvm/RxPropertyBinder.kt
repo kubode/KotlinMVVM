004b5543af17eb48d761babc9fff226aebbf741d
久保出雅俊 <kubode@team-lab.com>
2015-12-21 21:34:15 +0900

    KDoc追加

--- app/src/main/kotlin/com/teamlab/kotlin/mvvm/RxPropertyBinder.kt
+++ app/src/main/kotlin/com/teamlab/kotlin/mvvm/RxPropertyBinder.kt
@@ -1,10 +1,28 @@
 package com.teamlab.kotlin.mvvm
 
+import android.support.annotation.MainThread
 import android.widget.EditText
 import com.jakewharton.rxbinding.widget.textChanges
 import rx.Subscription
 import rx.android.schedulers.AndroidSchedulers
 
+/**
+ * [ReadOnlyRxProperty]を[V]にバインドする。
+ *
+ * メインスレッドにディスパッチされるので、[V]はUI以外指定しないこと。
+ *
+ * @param property
+ * バインドする[ReadOnlyRxProperty]。
+ * @param initializer
+ * [V]を[ReadOnlyRxProperty.value]で初期化するコールバック。
+ * [V]を`this`として扱う。
+ * このメソッドをコール時に処理するので、このメソッド自体をメインスレッドから呼ぶ必要がある。
+ * @param onNext
+ * [ReadOnlyRxProperty.publishSubject]の通知時に呼ばれるコールバック。
+ * [V]を`this`として扱う。
+ * メインスレッドにディスパッチされてコールされる。
+ */
+@MainThread
 fun <V, T> V.bindRxProperty(property: ReadOnlyRxProperty<T>,
                             initializer: V.(T) -> Unit,
                             onNext: V.(T) -> Unit): Subscription {
@@ -15,9 +33,14 @@ fun <V, T> V.bindRxProperty(property: ReadOnlyRxProperty<T>,
     return subscription
 }
 
+/**
+ * @see bindRxProperty
+ */
+@MainThread
 fun <V, T> V.bindRxProperty(property: ReadOnlyRxProperty<T>,
                             setter: V.(T) -> Unit) = bindRxProperty(property, setter, setter)
 
+@MainThread
 fun EditText.bindTextChanges(property: RxProperty<CharSequence>): Subscription {
     val subscription = textChanges().subscribe { property.value = it }
     setText(property.value)
