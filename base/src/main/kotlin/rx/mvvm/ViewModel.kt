2656d1a9c70cd46a52ec28c61373dfa86eedc76b
kubode <kubode@team-lab.com>
2016-01-12 02:04:29 +0900

    simplify

--- base/src/main/kotlin/rx/mvvm/ViewModel.kt
+++ base/src/main/kotlin/rx/mvvm/ViewModel.kt
@@ -1,29 +1,12 @@
 package rx.mvvm
 
 import android.os.Bundle
-import rx.subscriptions.CompositeSubscription
 
 abstract class ViewModel {
-
-    private val subscription = CompositeSubscription()
-
-    fun restoreInstanceState(savedInstanceState: Bundle?) {
-        savedInstanceState ?: return
-        onRestoreInstanceState(savedInstanceState)
-    }
-
+    fun restoreInstanceState(savedInstanceState: Bundle?) = savedInstanceState?.let { onRestoreInstanceState(it) }
     open protected fun onRestoreInstanceState(savedInstanceState: Bundle) = Unit
-
-    fun saveInstanceState(outState: Bundle) {
-        onSaveInstanceState(outState)
-    }
-
+    fun saveInstanceState(outState: Bundle) = onSaveInstanceState(outState)
     open protected fun onSaveInstanceState(outState: Bundle) = Unit
-
-    fun destroy() {
-        subscription.unsubscribe()
-        onDestroy()
-    }
-
+    fun destroy() = onDestroy()
     open protected fun onDestroy() = Unit
 }
