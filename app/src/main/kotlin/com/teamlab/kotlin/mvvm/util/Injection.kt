590d96ec4563225407b9c0c972a94000eca117ca
久保出雅俊 <kubode@team-lab.com>
2016-01-19 10:53:25 +0900

    内部仕様変更 @Namedは削除 Simplify

--- app/src/main/kotlin/com/teamlab/kotlin/mvvm/util/Injection.kt
+++ app/src/main/kotlin/com/teamlab/kotlin/mvvm/util/Injection.kt
@@ -3,49 +3,29 @@ package com.teamlab.kotlin.mvvm.util
 import java.util.*
 import kotlin.reflect.KClass
 
-/**
- * Implements this interface if has [ObjectGraph].
- */
-interface HasObjectGraph {
-    /**
-     * Define [ObjectGraph].
-     */
-    val objectGraph: ObjectGraph
+internal interface Provider<T> {
+    fun get(objectGraph: ObjectGraph): T
 }
 
-private interface Wrapper<V> {
-    val value: V
-}
+internal class SingletonProvider<T>(private val initializer: ObjectGraph.() -> T) : Provider<T> {
+    private object EMPTY
 
-private class NormalWrapper<V>(private val objectGraph: ObjectGraph,
-                               private val initializer: ObjectGraph.() -> V) : Wrapper<V> {
-    override val value: V
-        get() = initializer(objectGraph)
+    @Suppress("UNCHECKED_CAST")
+    private var value = EMPTY as T
 
-    override fun toString(): String {
-        return "$value"
+    override fun get(objectGraph: ObjectGraph): T {
+        if (value == EMPTY) {
+            value = initializer(objectGraph)
+        }
+        return value
     }
 }
 
-private class SingletonWrapper<V>(private val objectGraph: ObjectGraph,
-                                  private val initializer: ObjectGraph.() -> V) : Wrapper<V> {
-    override val value: V by lazy { initializer(objectGraph) }
-    override fun toString(): String {
-        return "$value(Singleton)"
+internal class NormalProvider<T>(private val initializer: ObjectGraph.() -> T) : Provider<T> {
+    override fun get(objectGraph: ObjectGraph): T {
+        return initializer(objectGraph)
     }
 }
-
-private enum class Type(private val wrapper: (ObjectGraph, ObjectGraph.() -> Any) -> Wrapper<*>) {
-    NORMAL({ objectGraph, initializer -> NormalWrapper(objectGraph, initializer) }),
-    SINGLETON({ objectGraph, initializer -> SingletonWrapper(objectGraph, initializer) });
-
-    internal fun wrap(objectGraph: ObjectGraph, initializer: ObjectGraph.() -> Any): Wrapper<*> {
-        return wrapper(objectGraph, initializer)
-    }
-}
-
-private class Provider<V : Any>(internal val type: Type,
-                                internal val initializer: ObjectGraph.() -> V)
 
 /**
  * Define module by extend this class like below.
@@ -54,19 +34,17 @@ private class Provider<V : Any>(internal val type: Type,
  * class MyModule : Module() {
  *     init {
  *         provide(Double::class, { Math.random() })
- *         provideSingleton(Int::class, { 1 }, "named")
  *         provideSingleton(String::class, { "Dependency: ${get(Int::class}" })
  *     }
  * }
  * ```
  */
-public abstract class Module {
-    internal val includes: MutableList<Module> = LinkedList()
-    @Suppress("EXPOSED_PROPERTY_TYPE")
-    internal val providers: MutableMap<Class<*>, MutableMap<String?, Provider<*>>> = HashMap()
+abstract class Module {
+    internal val includes = LinkedList<Module>()
+    internal val providers: MutableMap<KClass<*>, Provider<*>> = HashMap()
 
-    private fun <V : Any> add(clazz: KClass<V>, name: String? = null, provider: Provider<*>) {
-        providers.getOrPut(clazz.java, { HashMap() }).put(name, provider)
+    internal fun <T : Any> add(clazz: KClass<T>, provider: Provider<*>) {
+        providers.getOrPut(clazz, { provider })
     }
 
     /**
@@ -79,15 +57,15 @@ public abstract class Module {
     /**
      * Provide object.
      */
-    protected fun <V : Any> provide(clazz: KClass<V>, initializer: ObjectGraph.() -> V, name: String? = null) {
-        add(clazz, name, Provider(Type.NORMAL, initializer))
+    protected fun <T : Any> provide(clazz: KClass<T>, initializer: ObjectGraph.() -> T) {
+        add(clazz, NormalProvider(initializer))
     }
 
     /**
      * Provide object as singleton.
      */
-    protected fun <V : Any> provideSingleton(clazz: KClass<V>, initializer: ObjectGraph.() -> V, name: String? = null) {
-        add(clazz, name, Provider(Type.SINGLETON, initializer))
+    protected fun <T : Any> provideSingleton(clazz: KClass<T>, initializer: ObjectGraph.() -> T) {
+        add(clazz, SingletonProvider(initializer))
     }
 }
 
@@ -96,19 +74,15 @@ public abstract class Module {
  *
  * @constructor Creates object graph that inherited [parent].
  */
-public class ObjectGraph(private val parent: ObjectGraph? = null) {
-    private val graph: MutableMap<Class<*>, MutableMap<String?, Wrapper<*>>> = HashMap()
+class ObjectGraph(private val parent: ObjectGraph? = null) {
+    private val graph: MutableMap<KClass<*>, Provider<*>> = HashMap()
 
     /**
      * Add new [Module] to this.
      */
-    public fun add(module: Module): ObjectGraph {
+    fun add(module: Module): ObjectGraph {
         module.includes.forEach { add(it) }
-        module.providers.forEach {
-            graph.getOrPut(it.key, { HashMap() }).putAll(it.value.mapValues {
-                it.value.type.wrap(this, it.value.initializer)
-            })
-        }
+        graph.putAll(module.providers)
         return this
     }
 
@@ -118,16 +92,25 @@ public class ObjectGraph(private val parent: ObjectGraph? = null) {
      *
      * @throws RuntimeException When object is not found.
      */
-    public fun <V : Any> get(clazz: KClass<V>, name: String? = null): V {
+    public fun <T : Any> get(clazz: KClass<T>): T {
         @Suppress("UNCHECKED_CAST")
-        return graph[clazz.java]?.get(name)?.value as V?
-                ?: parent?.get(clazz, name)
-                ?: throw RuntimeException("${clazz.java}(${name ?: "No name"}) is not found.")
+        return graph[clazz]?.get(this) as T?
+                ?: throw RuntimeException("$clazz is not found.")
     }
 
     override fun toString(): String {
         return "{parent: $parent, graph: {$graph}}"
     }
+}
+
+/**
+ * Implements this interface if has [ObjectGraph].
+ */
+interface HasObjectGraph {
+    /**
+     * Define [ObjectGraph].
+     */
+    val objectGraph: ObjectGraph
 }
 
 class HasObjectGraphFinder(internal vararg val finders: () -> Any?) {
@@ -150,4 +133,4 @@ private val objectGraphCache = WeakHashMap<Injectable, ObjectGraph>()
 val Injectable.parentObjectGraph: ObjectGraph
     get() = objectGraphCache.getOrPut(this, { findObjectGraph() })
 
-fun <V : Any> Injectable.inject(clazz: KClass<V>, name: String? = null) = lazy { parentObjectGraph.get(clazz, name) }
+fun <T : Any> Injectable.inject(clazz: KClass<T>) = lazy { parentObjectGraph.get(clazz) }
