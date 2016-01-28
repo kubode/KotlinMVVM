ed752df593a958f27ffc527da73e40d2a1b886cd
久保出雅俊 <kubode@team-lab.com>
2016-01-28 20:39:09 +0900

    remove twitter()

--- app/src/main/kotlin/com/teamlab/kotlin/mvvm/di/MyApplicationComponent.kt
+++ app/src/main/kotlin/com/teamlab/kotlin/mvvm/di/MyApplicationComponent.kt
@@ -8,7 +8,6 @@ import com.teamlab.kotlin.mvvm.view.AccountsFragment
 import com.teamlab.kotlin.mvvm.view.AddAccountDialogFragment
 import com.teamlab.kotlin.mvvm.view.MainActivity
 import dagger.Component
-import twitter4j.Twitter
 import javax.inject.Singleton
 
 @Singleton
@@ -18,8 +17,6 @@ interface MyApplicationComponent {
     fun inject(mainActivity: MainActivity)
     fun inject(accountsFragment: AccountsFragment)
     fun inject(addAccountDialogFragment: AddAccountDialogFragment)
-
-    fun twitter(): Twitter
 
     companion object {
         fun from(context: Context) = (context.applicationContext as MyApplication).component
