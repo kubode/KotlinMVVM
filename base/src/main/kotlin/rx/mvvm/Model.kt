a6560506e5ca67918d61ab26606729b25cdd470d
久保出雅俊 <kubode@team-lab.com>
2016-02-22 15:44:40 +0900

    modify shorthand method declaring to curly brace declaring.

--- base/src/main/kotlin/rx/mvvm/Model.kt
+++ base/src/main/kotlin/rx/mvvm/Model.kt
@@ -1,18 +1,22 @@
 package rx.mvvm
 
-abstract class Model<K> {
+abstract class Model<K : Any> {
+
     abstract val id: K
+
     final override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (other?.javaClass != javaClass) return false
-
         other as Model<*>
-
         if (id != other.id) return false
-
         return true
     }
 
-    final override fun hashCode() = id?.hashCode() ?: 0
-    override fun toString() = "${javaClass.simpleName}($id)"
+    final override fun hashCode(): Int {
+        return id.hashCode()
+    }
+
+    override fun toString(): String {
+        return "${javaClass.simpleName}($id)"
+    }
 }
