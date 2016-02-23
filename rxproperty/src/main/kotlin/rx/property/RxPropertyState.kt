b402f72d4e4f1e0404ff742fc5c42cd4704069be
久保出雅俊 <kubode@team-lab.com>
2016-02-23 12:07:43 +0900

    Add comments.

--- rxproperty/src/main/kotlin/rx/property/RxPropertyState.kt
+++ rxproperty/src/main/kotlin/rx/property/RxPropertyState.kt
@@ -8,7 +8,15 @@ import rx.Subscriber
  */
 abstract class RxPropertyState<T> : Observable.OnSubscribe<T> {
 
+    /**
+     * [Observable]としての移譲先となるプロパティ。
+     * [rx.subjects.BehaviorSubject]の様に、Subscribe時に現在の値を返すようにする。
+     */
     abstract val observable: Observable<T>
+    /**
+     * プロパティ化した際の移譲先となるプロパティ。
+     * プロパティ化しない想定であれば、例外を投げても良い。
+     */
     abstract var value: T
 
     final override fun call(child: Subscriber<in T>) {
