cd522f9e6c369bacf1933b47b24302bdb1dd020e
久保出雅俊 <kubode@team-lab.com>
2016-03-07 12:48:51 +0900

    Add Picasso.

--- app/src/main/kotlin/com/teamlab/kotlin/mvvm/di/ApplicationModule.kt
+++ app/src/main/kotlin/com/teamlab/kotlin/mvvm/di/ApplicationModule.kt
@@ -2,6 +2,10 @@ package com.teamlab.kotlin.mvvm.di
 
 import android.content.Context
 import com.github.kubode.rxeventbus.RxEventBus
+import com.jakewharton.picasso.OkHttp3Downloader
+import com.squareup.picasso.LruCache
+import com.squareup.picasso.Picasso
+import com.teamlab.kotlin.mvvm.BuildConfig
 import com.teamlab.kotlin.mvvm.MyApplication
 import dagger.Module
 import dagger.Provides
@@ -13,4 +17,10 @@ class ApplicationModule(private val application: MyApplication) {
     @Provides @ApplicationScope fun provideContext(): Context = application
     @Provides @ApplicationScope fun provideRefWatcher() = application.ref
     @Provides @ApplicationScope fun provideRxEventBus() = RxEventBus()
+    @Provides @ApplicationScope fun providePicasso() = Picasso.Builder(application)
+            .indicatorsEnabled(BuildConfig.DEBUG)
+            .loggingEnabled(BuildConfig.DEBUG)
+            .downloader(OkHttp3Downloader(application))
+            .memoryCache(LruCache(application))
+            .build()
 }
